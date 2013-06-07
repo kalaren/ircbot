50132f2dae5f0144df228bd8a1698ec92c9eb18b
Eric Szeto <eszeto@gmail.com>
2013-06-07 12:25:40 -0400

    first commit i think

---
+++ ircbot.rb
@@ -0,0 +1,40 @@
+require "socket"
+
+
+class Headline
+
+	
+
+	def initialize()
+
+		@server = "chat.freenode.net"
+		@port = "6667"
+		@nick = "DrewEricSbot"
+		@channel = "#bitmaker"
+		@s = TCPSocket.open(@server, @port)
+		print("addr: ", @s.addr.join(":"), "\n")
+		print("peer: ", @s.peeraddr.join(":"), "\n")
+		@s.puts "USER eksdsbot 0 * eksdsbot"
+		@s.puts "NICK #{@nick}"
+		@s.puts "JOIN #{@channel}"
+		@s.puts "PRIVMSG #{@channel} :Hello from IRB Bot"
+	end
+
+	def send
+	end
+
+	def run
+
+		until @s.eof? do
+		  msg = @s.gets
+		  puts msg
+
+		  if msg.include? "headline"
+		  	@s.puts "PRIVMSG #{@channel} :The current headline at Toronto Star is: #{}"
+		  end
+		end
+	end
+end
+
+bot = Headline.new
+bot.run
